d2451ee41c99da49d60e6f51a36860111c87e891
krzysztof6563 <krzysztofmichalski42@gmail.com>
2026-03-31 20:37:35 +0200

    set lang to pl

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { useHead } from "@unhead/vue";
 import Footer from "./components/global/Footer.vue";
 import Header from "./components/global/Header.vue";
+
+useHead({
+    htmlAttrs: {
+        lang: "pl",
+    },
+});
 </script>
 
 <template>
